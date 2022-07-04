f125f56c05275fab5d807974eccb931c8a42f81d
shravanatirtha <shravz.ceo@gmail.com>
2022-07-04 15:05:05 +0530

    logical error

--- Array/MakeLargerNumber.java
+++ Array/MakeLargerNumber.java
@@ -12,6 +12,37 @@ public class MakeLargerNumber {
     }
 
     static int makeLargeNumber(int num) {
-        return num;
+        List list = numToList(num);
+        int right = -1;
+        for (int i = list.size() - 1; i > 0; i--) {
+            if (list.get(i) > list.get(i - 1)) {
+                right = i;
+                break;
+            }
+        }
+        if (right != -1) {
+            swap(list, right, right - 1);
+            sort(list, right, list.size());
+        }
+        return listToNum(list);
+    }
+
+    static List numToList(int num) {
+        List d = new ArrayList<>();
+        while (num > 0) {
+            d.add(0, num % 10);
+            num /= 10;
+        }
+        return d;
+    }
+
+    static void swap(List l, int a, int b) {
+        int temp = l.get(a);
+        l.set(a, l.get(b));
+        l.set(b, temp);
+    }
+
+    static int listToNum(int num){
+        return 
     }
 }
