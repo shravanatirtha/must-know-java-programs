000ca0a1aeccd8d5541d9e4ab2bc53dd3cfff85e
shravanatirtha <shravz.ceo@gmail.com>
2022-07-07 21:02:56 +0530

    added linked list reverse

---
+++ LinkedList/ReverseIterative.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+import java.util.*;
+
+public class ReverseIterative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+    static class Node{
+        public Node next;
+        int data;
+    }
+}
