a208bbe33f8637e619ea3f5e3637e745b7b0da2e
shravanatirtha <shravz.ceo@gmail.com>
2022-07-08 05:52:20 +0530

    swapping

--- Bits/Swap.java
+++ Bits/Swap.java
@@ -5,12 +5,23 @@ import java.util.*;
 public class Swap {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        num1 ^= num2;
-        num2 ^= num1;
-        num1 ^= num2;
+        int num1 = sc.nextInt(); // 2
+        int num2 = sc.nextInt(); // 3
+        num1 ^= num2; // a = a ^ b --- 0 0 0 1 = 1
+        num2 ^= num1; // b = a ^ b --- 0 0 1 0 = 2
+        num1 ^= num2; // a = a ^ b ---  0 0 1 1 = 3
         System.out.print(num1 + " " + num2);
         sc.close();
     }
 }
+/*
+
+exor
+0 0 1 0      0 0 0 1     0 0 0 1
+0 0 1 1      0 0 1 1     0 0 1 0
+----------   ---------  ---------
+0 0 0 1      0 0 1 0     0 0 1 1 
+
+ */
+
+
